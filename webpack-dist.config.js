284462518117886f29e1837dd8c7e455fde51131
qq240814476 <240814476@qq.com>
2019-08-26 22:12:06 +0800

    a

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -84,15 +84,6 @@ module.exports = {
       fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'malin-resume.site');
 
       await publishGhPages();
-
-      // 调用 Chrome 渲染出 PDF 文件
-      const chromePath = findChrome();
-      spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
-        'http://malin-resume.site' // 这里注意改成你的在线简历的网站
-      ]);
-
-      // 重新发布到 ghpages
-      await publishGhPages();
     }),
   ]
 };
