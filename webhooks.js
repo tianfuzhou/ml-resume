b7e5d02f229ef3e8b3fd90ebaa5c6e4bb34a3a35
qq240814476 <240814476@qq.com>
2020-03-03 10:08:51 +0800

    web

--- webhooks.js
+++ webhooks.js
@@ -1,10 +1,10 @@
 const http = require('http')
 const createHandler = require('github-webhook-handler')
 
-const handler = createHandler({
-    path:'/resume-hook',
-    secret:'malin258'
-})
+// const handler = createHandler({
+//     path:'/resume-hook',
+//     secret:'malin258'
+// })
 
 function run_cmd(cmd, args, callback) {
   var spawn = require('child_process').spawn;
@@ -18,24 +18,32 @@ function run_cmd(cmd, args, callback) {
   });
 }
 
-http.createServer((req,res) => {
-  handler(req,res,err => {
-    res.statusCode = 404
-    res.end('no such location')
-  })
-}).listen(7778, () => {
-  console.log('Webhook listen at 7778')
+// http.createServer((req,res) => {
+//   handler(req,res,err => {
+//     res.statusCode = 404
+//     res.end('no such location')
+//   })
+// }).listen(7778, () => {
+//   console.log('Webhook listen at 7778')
+// })
+http.createServer((req,res)=>{
+  console.log('请求',req)
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('响应内容');
+})
+http.listen(8000, () => {
+  console.log('Webhook listen at 8000')
 })
 
-handler.on('error',err => {
-  console.error('Error',err.message)
-})
+// handler.on('error',err => {
+//   console.error('Error',err.message)
+// })
 
-handler.on('*', function (event) {
-  console.log('Received a push event for %s to %s', event.payload.repository.name, event.payload.ref);
-  // 分支判断
-  if(event.payload.ref === 'refs/heads/master'){
-    console.log('deploy master..')
-    run_cmd('sh', ['./autoDeploy.sh'], function(text){ console.log(text) });
-  }
-})
+// handler.on('*', function (event) {
+//   console.log('Received a push event for %s to %s', event.payload.repository.name, event.payload.ref);
+//   // 分支判断
+//   if(event.payload.ref === 'refs/heads/master'){
+//     console.log('deploy master..')
+//     run_cmd('sh', ['./autoDeploy.sh'], function(text){ console.log(text) });
+//   }
+// })
